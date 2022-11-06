0283f17c2f6b86e0dfb546a57ad667558ea9c464
Muhammad Nur Ilham <muhnilham99@gmail.com>
2022-11-07 01:32:46 +0700

    update

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -16,7 +16,7 @@ func DatabaseInit() {
 	var DB_HOST = os.Getenv("DB_HOST")
 	var DB_USER = os.Getenv("DB_USER")
 	var DB_PASSWORD = os.Getenv("DB_PASSWORD")
-	var DB_NAME = os.Getenv("GET_NAME")
+	var DB_NAME = os.Getenv("DB_NAME")
 	var DB_PORT = os.Getenv("DB_PORT")
 
 	// dsn := "root:@tcp(127.0.0.1:3306)/be-waysfood?charset=utf8mb4&parseTime=True&loc=Local"
